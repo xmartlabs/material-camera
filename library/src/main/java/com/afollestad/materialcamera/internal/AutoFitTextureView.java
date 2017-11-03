26c381d24b6844d00f312c3d9b73993859565d98
Mike <mike@xmartlabs.com>
2017-11-03 11:48:31 -0300

    Fixing public class

--- library/src/main/java/com/afollestad/materialcamera/internal/AutoFitTextureView.java
+++ library/src/main/java/com/afollestad/materialcamera/internal/AutoFitTextureView.java
@@ -21,7 +21,7 @@ import android.util.AttributeSet;
 import android.view.TextureView;
 
 /** A {@link TextureView} that can be adjusted to a specified aspect ratio. */
-class AutoFitTextureView extends TextureView {
+public class AutoFitTextureView extends TextureView {
 
   private int mRatioWidth = 0;
   private int mRatioHeight = 0;
