f4bac95fb3a0e239b33084f1d995bf4a7bef6929
Santiago Castro <santiago@xmartlabs.com>
2017-11-30 14:06:16 -0300

    Fix aspect ratio

--- library/src/main/java/com/afollestad/materialcamera/internal/AutoFitTextureView.java
+++ library/src/main/java/com/afollestad/materialcamera/internal/AutoFitTextureView.java
@@ -54,4 +54,20 @@ public class AutoFitTextureView extends TextureView {
     mRatioHeight = height;
     requestLayout();
   }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    int width = MeasureSpec.getSize(widthMeasureSpec);
+    int height = MeasureSpec.getSize(heightMeasureSpec);
+    if (0 == mRatioWidth || 0 == mRatioHeight) {
+      setMeasuredDimension(width, height);
+    } else {
+      if (width < height * mRatioWidth / mRatioHeight) {
+        setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+      } else {
+        setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+      }
+    }
+  }
 }
